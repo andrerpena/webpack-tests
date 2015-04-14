f1d87854e83f4a528d69eafc00b8ab2cad417fd2
andrerpena <andrerpena@gmail.com>
2015-04-13 21:14:13 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: "./scripts/entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
